e6249af74aa428e38224a7a39bffd3c2ad5b647d
Adrianumpad <adrianumpad310@gmail.com>
2025-09-02 23:31:28 +0800

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,9 @@
+# Write code below 💖
+
+co = int(input("enter amount in colombian pesos:"))
+pe = int(input("enter amount in peruvian sales:"))
+br = int(input("enter amoutt in brazilian reals:"))
+
+amount = co * 0.00025 + pe * 0.28 + br * 0.18
+
+print(amount)
