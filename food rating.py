9f9dd4abb2a2f513ded40c0883bcc9b2d9d9c0cd
Adrianumpad <adrianumpad310@gmail.com>
2025-09-08 21:55:02 +0800

    Create food rating.py

---
+++ food rating.py
@@ -0,0 +1,12 @@
+rating = float(input("input your rating: "))
+
+if rating > 4.5:
+  print("Extraordinary")
+elif rating > 4:
+  print("Excellent")
+elif rating > 3:
+  print("good")
+elif rating > 2:
+  print("fair")
+else:
+  print("Poor")
